104564742138b8d032f4a91a21e472a801c44d85
Jake Brukhman <jake@coinfund.io>
2021-08-03 21:08:11 -0400

    First colorful version 1

--- jbrukh/ouroboros/sketch.js
+++ jbrukh/ouroboros/sketch.js
@@ -1,23 +1,37 @@
 let t = 0;
 let px, py;
 let squish = 1.4;
-let iter = 135000;
-let ptsPerLoop = 800;
+let iter = 175000;
+let ptsPerLoop = 1600;
 let i = 0;
 let col1, col2;
 let loopStyle;
 let xSwivel, ySwivel;
 let noLoopMode = false;
-let capturer = new CCapture({ format: 'png', framerate: 60 });
+let capturer = new CCapture({ format: 'gif', workersPath: '../libraries/', framerate: 60 });
+let record = false;
+let p1, p2, p3, p4;
+let coolOnes = [10, 23, 213];
 
 function setVars(seed) {
 	randomSeed(seed);
-	squish 		= random(1, 2);
-	loopStyle = random(20, 120);
-	col1 			= color(random(255), random(255), random(255));
-	col2 			= color(random(255), random(255), random(255));
+	squish 		= random(2.5, 4);
+	loopStyle   = random(20, 120);
+	col1 		= color(random(255), random(255), random(255));
+	col2 		= color(random(255), random(255), random(255));
 	xSwivel 	= random();
 	ySwivel 	= random();
+
+	// random polarity
+	p1 			= random() > 0.5 ? -1 : 1;
+	p2 			= random() > 0.5 ? -1 : 1;
+	p3 			= random() > 0.5 ? -1 : 1;
+	p4 			= random() > 0.5 ? -1 : 1;
+	
+	// random rotation
+	r1          = random() * TWO_PI;
+	
+	print('squish = %f, loopStyle = %f', squish, loopStyle);
 }
 
 function setup() {
@@ -25,34 +39,48 @@ function setup() {
 	background(0);
 	stroke(255);
 	strokeWeight(1); 
-	setVars(13);	
+	setVars(222);	
 }
 
 function x(t) {
-	return sin(t / squish)*200 + 3*t*sin(loopStyle*t) + xSwivel * t;
+	return sin(p1 * t / squish)*200 + 2.5*t*sin(loopStyle * t * p2) + xSwivel * t;
 }
 
 function y(t) {
-	return cos(t / squish)*200 + 3*t*cos(loopStyle*t);
+	return cos(p3 * t / squish)*200 + 2.5*t*cos(loopStyle * t * p4);
 }
 
 function makeSnake() {
-	if (frameCount === 1) {
+	if (record && frameCount === 1) {
 		capturer.start();
 	}
+	if (record && i >= iter) {
+		noLoop();
+		capturer.stop();
+		capturer.save();
+		return;
+	}
 	translate(width/2, height/2);	
+	rotate(r1);
 	for (var j = 0; j < ptsPerLoop; j++, i++) {
 		let xt = x(t), yt = y(t);
 		if (px && py) {
 			strokeWeight(1.5*(1-i/iter));
 			let c = lerpColor(col1, col2, i/iter);
-			c.setAlpha(0.9 * 255 * (1-i/iter)^2);
+			c.setAlpha(0.90 * 255 * (1-i/iter)^2);
+			c.setRed(c._getRed() * noise(t/10));
+			c.setGreen(c._getGreen() * noise(t/7+7));
+			c.setBlue(c._getBlue() * noise(t/10+11));
 			stroke(c);
 			line(px, py, xt, yt);
 		}
 		px = xt;
 		py = yt;
 		t += 0.0005;
+	}
+
+	if (record) {
+		capturer.capture(document.getElementById('defaultCanvas0'));
 	}
 }
 
